b0a9c51940dc0bf4f3838a96f4b361829d1e096c
Jakub Jurczynski <JJURCZYNSKI@foss.pl>
2019-10-11 15:34:32 +0200

    Updates

---
+++ precache-manifest.93d691526d20e8cb2067ca56727725d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10da953f8c5eb884b83de9ae064eefe2",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "4dcf5c2612783d1ecf0c",
+    "url": "/monsters-rolodex-react/static/css/main.fb5f0bca.chunk.css"
+  },
+  {
+    "revision": "f0fd004aafba1ae4d4bd",
+    "url": "/monsters-rolodex-react/static/js/2.702c9599.chunk.js"
+  },
+  {
+    "revision": "4dcf5c2612783d1ecf0c",
+    "url": "/monsters-rolodex-react/static/js/main.8ce6f1b5.chunk.js"
+  },
+  {
+    "revision": "8a3e76548a558407645c",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.d1f0707d.js"
+  }
+]);
